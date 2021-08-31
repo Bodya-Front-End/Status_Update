a725f0deaac4dd37aa6b8904bd8b57e6b1950c62
Bodya <bodya@gmail.com>
2021-08-31 18:50:07 +0300

    refactoring extra Space and add comments

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,38 +12,45 @@ import {MatTableDataSource} from "@angular/material/table";
 })
 export class MainComponent implements OnInit, OnDestroy {
   public loading = false;
-  public gotAnAnswer = true;
+  public gotAnAnswer = false;
+  // create Enum for change color text
   public responseStatus = ResponseStatus;
   public answer: LogData;
   private destroy$ = new Subject();
+
+  // variables for table
   public displayedColumns: string[] = ['date', 'url', 'result'];
-  private logs  = [];
   public dataSourceTable = new MatTableDataSource();
+  private logs  = [];
 
 
-  constructor(private sendRequestService: SendRequestService) {
-  }
+  constructor(private sendRequestService: SendRequestService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy() {
+    //unsubscribe for all subscriptions
     this.destroy$.next(true);
     this.destroy$.complete();
   }
 
   public sendRequest() {
+    // turn on spinner
     this.loading = true
     this.sendRequestService.getStatus()
       .pipe(
         takeUntil(this.destroy$),
         finalize(() => {
+          // turn off spinner
           this.loading = false
+          // variable for show right card
           this.gotAnAnswer = true;
         })
       )
       .subscribe(res => {
+        //variable for data right card
         this.answer = res;
+        //update the data for the table
         this.logs.push(res)
         this.dataSourceTable.data = this.logs
       })
--- src/app/services/send-request.service.ts
+++ src/app/services/send-request.service.ts
@@ -3,16 +3,18 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 
-
+// this interface for get request
 export interface Response {
   status: string
 }
+
+// this interface for create request object
 export interface LogData {
   date: Date;
   url: string;
   result: string;
 }
-
+// this enum for add color text
 export enum ResponseStatus {
   SUCCESS = 'Success',
   PENDING = 'Pending',
@@ -22,18 +24,21 @@ export enum ResponseStatus {
   providedIn: 'root'
 })
 export class SendRequestService {
+  // create Headers for request
   private readonly headers = new HttpHeaders({
     'x-api-key': 'syaNs11gHB9pev48g5Zrt5p5O4cKX7yU1bUDj7F5'
   });
 
   private readonly url: string;
   constructor(private http: HttpClient) {
+    //initialize URL
     this.url = 'https://c5ljdx2w0m.execute-api.eu-central-1.amazonaws.com/recruitment/'
   }
 
   public getStatus(): Observable<LogData> {
-
+    //submitting a request
     return this.http.get<Response>(this.url, {headers: this.headers})
+      //modify the answer for the table
       .pipe(
         map((res) => {
           return {
